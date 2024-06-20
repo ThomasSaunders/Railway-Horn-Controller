a1f1ac87b94407384ce49cbdb0d0fae25771b2ec
Thomas Saunders <psn.github@ts379.co.uk>
2024-06-20 15:49:28 +0100

    Basic horn control
    Now need solenoid and control buttons.

--- Railway-Horn-Controller.ino
+++ Railway-Horn-Controller.ino
@@ -1,26 +1,65 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
+// Pins for Solenoid control
+const int LOW_HORN = 2;
+const int HIGH_HORN = 3;
+
+// Pins for High/Low manual control buttons
+const int LOW_CONTROL = 4;
+const int HIGH_CONTROL = 5;
+
+static LiquidCrystal_I2C lcd(0x27, 20, 4);
+
 void setup() {
   Serial.begin(9600);
 
+  // Use built in LED for status checking 
   pinMode(LED_BUILTIN, OUTPUT);
+  
+  pinMode(LOW_HORN, OUTPUT);
+  pinMode(HIGH_HORN, OUTPUT);
 
-  static LiquidCrystal_I2C lcd(0x27, 20, 4);
+  pinMode(LOW_CONTROL, INPUT);
+  pinMode(HIGH_CONTROL, INPUT);
 
   lcd.init();
   lcd.clear();
   lcd.backlight();
 
   lcd.print("Railway Horn Controller");
-
-  delay(1000);
-
-  lcd.clear();
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+ manualControl();
+
+ delay(1500);
+
+}
+
+void manualControl() {
+  lcd.clear();
+
+  lcd.print("Manual Control");
+  lcd.setCursor(1, 0);
+
+  while(true) {
+    if(digitalRead(LOW_CONTROL) == HIGH){
+      lcd.print("Low Horn");
+      digitalWrite(LOW_HORN, HIGH);
+    } else if (digitalRead(HIGH_CONTROL) == HIGH) {
+      lcd.print("High Horn");
+      digitalWrite(HIGH_HORN, HIGH);
+    } else {
+      lcd.setCursor(0, 0);
+      lcd.write("Manual Control");
+      lcd.setCursor(1, 0);
+
+      digitalWrite(LOW_HORN, LOW);
+      digitalWrite(HIGH_HORN, LOW);
+    }
+
+    delay(250);
+  }
 }
